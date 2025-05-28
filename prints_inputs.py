96cd5e2578d39bd35410958043a9072307f7e42b
LuckyVellOTD <lavelledjr@gmail.com>
2025-05-27 20:00:58 -0400

    Add files via upload

---
+++ prints_inputs.py
@@ -0,0 +1,234 @@
+# print("\033c")
+# print("Welcome!!! \nI'm Extremely Lost") 
+# print("Hey Look! \nI'm able to make \nseparate lines like \na normal person.")
+# print("Ok, enough playing around...Here we go!")
+# fname = input("What is your name?")
+# print(f"Hello, {fname}!")
+# x=input("What's your favorite number?")
+
+
+# number_of_magic_beans = 5
+# price_per_bean = 3
+# tot_price = number_of_magic_beans * price_per_bean
+# print("The total price:")
+# print(tot_price)
+
+
+# apples = 3
+# bananas = 5
+# fruit_total = 3 + 5
+# print("Total number of fruits:")
+# print("8")
+
+
+# distance = 50
+# time = 5
+# speed = 50/5
+# print("The speed is...")
+# print(speed)
+
+
+# number_of_drinks = 10
+# number_of_spilled_drinks = 5
+# tot_number_of_drinks = number_of_drinks - number_of_spilled_drinks
+# print("Total number of purchasable drinks... ")
+# print(tot_number_of_drinks)
+
+
+# greeting ="Hello"
+# customer ="Bob"
+# combined = greeting + customer
+# print(combined)
+
+
+# shopper = "bob"
+# shouted = shopper.upper()
+# print(shouted)
+
+
+# greeted_shop = "HelLO BOb"
+# no_cap = greeted_shop.lower()
+# print(no_cap)
+
+
+# greet = "HelLOoOooOO"
+# capi = greet.capitalize()
+# print(capi)
+
+
+# merchant ="caTHy"
+# shouted_cust=merchant.upper()
+# print("The merchant's name in all uppercase is...")
+# print(shouted_cust)
+# print("The merchant's name in all lowercase is...")
+# print(merchant.lower())
+# print("The merchant's name in proper capitalization is...")
+# print(merchant.capitalize())
+
+
+# a_long_word="antidisestablishmentarianism"
+# print("The length of the the long word is...")
+# print(len(a_long_word))
+# print("The length of the word Hi is...")
+# print(len("hi"))
+
+
+# grapes = 3
+# print("I have", grapes, "grapes.")
+
+
+# kiwis = 3
+# print(f"I have {kiwis} kiwis.")
+
+
+# melons = 3
+# print(melons)
+
+
+# print("Enter the customer name: ")
+# buyer = input()
+# salutation = "hello" + buyer + "!"
+# print(salutation.upper())
+
+
+# print("Enter a color: ")
+# color=input()
+# print("Enter an animal: ")
+# animal=input()
+# print("Enter a plant: ")
+# plant =input()
+# print("The color you chose is",color)
+# print("The animal you chose is", animal)
+# print("The plant you chose is", plant)
+
+
+# print("Enter the name of a car.")
+# car=input()
+# print("The car you named is",car)
+# length_carname = len(car)
+# print("That car's name contains", length_carname, "characters.")
+
+
+# print("Enter the name of a planet")
+# planet=input()
+# print("That Planet's name in all capital letters would be...")
+# print(planet.upper())
+
+
+# print("Name a planet")
+# planet=input()
+# length_planet_name=len(planet)
+# print("That planet's name is", length_planet_name, "characters long.")
+
+
+# print("Enter an animal's name")
+# Animal=input()
+# print("The name of the animal you chose is",Animal , "I think it would make a good pet!")
+
+
+# print('Enter the name of an animal')
+# animal=input()
+# print('Enter the name of a plant')
+# plant=input()
+# print('The ', animal, ' eats the ', plant, 'everyday')
+
+
+
+# shade = input("Enter a color: ")
+# print("The color you chose is", shade)
+
+
+# pears = 3
+# print("I have", pears, "pears.")
+# onemore = pears + 1
+# print("If I had another, I would have", onemore, "pears.")
+# planet = "Mars"
+# print("Here's a planet name:", planet)
+# followed = planet + "1"
+# print("Here's that planet's name, followed by the number 1:", followed)
+
+# space = "Mars"
+# plums = 3
+# print(space + plums)
+
+
+# print("Welcome to the number adder that doesn't work right!")
+# a = input("What's a number you like? ")
+# b = input("Can you give me another number you like? ")
+# c = a + b
+# print("I added them. Here's what I got...")
+# print(c)
+
+
+# print("Welcome to the number adder that works well!")
+# a = int(input("What's a number you like? "))
+# b = int(input("Can you give me another number you like? "))
+# c = a + b
+# print(c)
+
+
+
+# a = int(input("What is a number you like? "))
+# b = int(input("Can you give me another number you like? "))
+# c = a + b
+# print(c)
+
+
+
+# favnum = int(input("What is your favorite number? "))
+# onemore = favnum + 1
+# print("One more would be", onemore)
+
+
+## 30
+## Ask the user for an integer.
+## Display "That number plus 2 is ___".
+
+
+# integer=int(input('Enter an interger please'))
+# plus_two= integer + 2
+# print(f"That number plus 2 is {plus_two}")
+
+
+# b=int(input("Please enter another number"))
+# c=a-b
+# print("The first number minus the second number would be...", c)
+
+      
+# a=int(input('Please input a number'))
+# b=a/2
+# print("Half of the number is", b)
+
+# print("Hello"*3)
+
+
+# a=input("Put in something")
+# print(a*3)
+
+
+
+## 38
+## Does this example do what you expect?
+## Why does it repeat the number five times rather than doing "real" math multiplication?
+## How do you make it do math?
+num = input("Give me a number. ")
+print("I'm going to try to multiply that number by 5,")
+print("but something strange is going to happen:")
+print(num*5)
+
+
+## 39
+## Ask the user for name and age.
+## Display "Guess what, ___, in two years you'll be ___."
+## (The user-provided name goes in the first blank, and the
+##   age two years from now in the second blank.)
+
+
+## 40
+## Ask the user for the number of questions on a test,
+## and ask "how many did you get right?"
+## Then, display "You got ___% right". (You'll need to calculate the percent.)
+
+########################
+##  INSTRUCTOR-CHECK  ##
+########################
